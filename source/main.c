ea7bf5b1edb4a1912e7ad3e3553493ad08ba2bc3
Squirrel <squirrelhomebrew@gmail.com>
2025-07-27 16:35:21 -0700

    Create main.c

---
+++ source/main.c
@@ -0,0 +1,21 @@
+#include <3ds.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    gfxInitDefault();
+    consoleInit(GFX_TOP, NULL);
+
+    printf("hello\n");
+
+    while (aptMainLoop()) {
+        hidScanInput();
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_START) break;
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+        gspWaitForVBlank();
+    }
+
+    gfxExit();
+    return 0;
+}
